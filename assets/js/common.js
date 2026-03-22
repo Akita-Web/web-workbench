fa5b8b6319466faeeb93961caab1d28039b144b0
Akita-Web <up5down46@gmail.com>
2026-03-22 09:20:25 +0900

    26/03/22

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,11 @@
 //ハンバーガーボタン開閉
-const hamburger_icon = document.querySelector('.hamburger-icon');
+const hamburger_btn = document.querySelector('.hamburger-btn');
 const g_nav = document.querySelector('.g-nav');
 
 console.log(g_nav);
-console.log(hamburger_icon);
+console.log(hamburger_btn);
 
-hamburger_icon.addEventListener('click', () =>{
-    hamburger_icon.classList.toggle('is-open');
+hamburger_btn.addEventListener('click', () =>{
+    hamburger_btn.classList.toggle('is-open');
     g_nav.classList.toggle('is-open');
 });
